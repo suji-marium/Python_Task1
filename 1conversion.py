184fccb40b58521cbab1eecf84f87a51a3c56140
suji-marium <[sujims1969@gmail.com]>
2024-09-04 11:50:59 +0530

    commit

--- 1conversion.py
+++ 1conversion.py
@@ -1,7 +1,20 @@
-x=float(input("Enter the fahrenheit temperature: "))
+"""x=float(input("Enter the fahrenheit temperature: "))
 y=((x-32)*5)/9
 
 print("The temperature in celsius: ",y)
 
 z=(y*(9/5))+32
-print("The temperature in fahrenheit",z)
+print("The temperature in fahrenheit",z)"""
+
+class Conversion:
+
+    def __init__(self,temp):
+        self.temp=temp
+    def fah_to_cel(self):
+        cel=((self.temp-32)*5)/9
+        fah=(cel*(9/5))+32
+        print("The temperature in celsius:",cel)
+        print("The temperature in fahrenheit", fah)
+
+con=Conversion(float(input('Enter the temperature: ')))
+con.fah_to_cel()
